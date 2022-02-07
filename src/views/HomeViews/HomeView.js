390770a1f253c2ce959fe3f101a815526a12677d
Dima Tytenko <amgpetronass@gmail.com>
2022-02-07 18:09:49 +0200

    add

--- src/views/HomeViews/HomeView.js
+++ src/views/HomeViews/HomeView.js
@@ -1,21 +1,8 @@
 import './HomeViews.scss';
 
-const styles = {
-  container: {
-    minHeight: 'calc(100vh - 50px)',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontWeight: 500,
-    fontSize: 48,
-    textAlign: 'center',
-  },
-};
-
 const HomeView = () => (
   <div className="HomeView">
+    <div className="HomeView__img"></div>
     <h1 className="HomeView__title">Did you register ? Log in : Sign up;</h1>
   </div>
 );
